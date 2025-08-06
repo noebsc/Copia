845137231cb3c23e9085be176e2e43a31d0dca4e
Noé <besancon.noe@gmail.com>
2025-08-06 18:27:39 +0200

    Mise à jour de l'app Copia (front + back + config Firebase)

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TA_CLÉ_FIREBASE",
-  authDomain: "ton-projet.firebaseapp.com",
-  projectId: "ton-projet",
-  storageBucket: "ton-projet.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyCLRtmDo78Dbohu9NRD-lbAmUO-Htqre4I",
+  authDomain: "assistant-71122.firebaseapp.com",
+  databaseURL: "https://assistant-71122-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "assistant-71122",
+  storageBucket: "assistant-71122.firebasestorage.app",
+  messagingSenderId: "123623966806",
+  appId: "1:123623966806:web:4a53e74c6c2e44d8ac53f7"
 };
 
 const app = initializeApp(firebaseConfig);
